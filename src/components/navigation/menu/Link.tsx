d9e94a0a3048059bd15fe291de38e690a156aab9
Anthony Bobrov <anton.bobrov@hotmail.com>
2022-06-30 14:01:34 +0300

    Update menu icons

--- src/components/navigation/menu/Link.tsx
+++ src/components/navigation/menu/Link.tsx
@@ -5,9 +5,29 @@ import styles from './Link.module.scss';
 import iconHome from '@/img/menu/home.svg';
 import iconHomeActive from '@/img/menu/home_active.svg';
 
+import iconPools from '@/img/menu/pools.svg';
+import iconPoolsActive from '@/img/menu/pools_active.svg';
+
+import iconFarms from '@/img/menu/farms.svg';
+import iconFarmsActive from '@/img/menu/farms_active.svg';
+
+import iconStaking from '@/img/menu/staking.svg';
+import iconStakingActive from '@/img/menu/staking_active.svg';
+
+import iconIdo from '@/img/menu/ido.svg';
+import iconIdoActive from '@/img/menu/ido_active.svg';
+
 const iconSrcMap = new Map();
 iconSrcMap.set('home', iconHome);
 iconSrcMap.set('home_active', iconHomeActive);
+iconSrcMap.set('pools', iconPools);
+iconSrcMap.set('pools_active', iconPoolsActive);
+iconSrcMap.set('farms', iconFarms);
+iconSrcMap.set('farms_active', iconFarmsActive);
+iconSrcMap.set('staking', iconStaking);
+iconSrcMap.set('staking_active', iconStakingActive);
+iconSrcMap.set('ido', iconIdo);
+iconSrcMap.set('ido_active', iconIdoActive);
 
 export interface NavigationMenuLinkProps {
     href: string;

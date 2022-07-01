7265e48b5a0aec86a599a0bf7b07632f1758598b
Anthony Bobrov <anton.bobrov@hotmail.com>
2022-07-01 15:41:15 +0300

    Add IconMetamask

---
+++ src/components/icons/Metamask.tsx
@@ -0,0 +1,49 @@
+import React, { forwardRef, HTMLAttributes } from 'react';
+
+interface Props extends HTMLAttributes<SVGSVGElement> { }
+
+export const IconMetamask = forwardRef<
+SVGSVGElement,
+Props
+>((tagProps, ref) => (
+    <svg
+        ref={ref}
+        {...tagProps}
+        width="14"
+        height="14"
+        viewBox="0 0 14 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path fillRule="evenodd" clipRule="evenodd" d="M9.99415 12.5714L8.15766 13.0644V12.4161L8.01127 12.2676H6.95996V13.0238V13.5572H8.08446L9.46845 12.9496L9.99415 12.5714Z" fill="#CDBDB2" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M4.00586 12.5714L5.8024 13.0644V12.4161L5.95544 12.2676H7.0001V13.0238V13.5572H5.8756L4.49157 12.9496L4.00586 12.5714Z" fill="#CDBDB2" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M8.01086 11.1668L8.15725 12.416L7.97092 12.2674H6.02799L5.80176 12.416L5.95479 11.1668L6.25422 10.9778L7.75134 11.0116L8.01086 11.1668Z" fill="#393939" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M9.02309 2.69263L8.12482 4.81962L7.7123 11.0114H6.25508L5.80262 4.81962L4.97754 2.69263H9.02309Z" fill="#F89C35" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M12.9491 7.36535L14.0004 10.4444L11.3788 10.2891H9.69531V8.95889L9.7685 6.23096L10.1478 6.52806L12.9491 7.36535Z" fill="#F89D35" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M10.9659 7.70947L7.89844 7.78372L8.23776 9.37726L9.69498 8.99913L10.9659 7.70947Z" fill="#D87C30" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M10.9652 7.74316L9.69434 8.95854V10.174L10.9652 7.74316Z" fill="#EA8D3A" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M9.69479 8.99902L8.19767 9.37711L7.71191 11.0112L8.05128 11.2003L9.69479 10.2144V8.99902Z" fill="#F89D35" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M9.69513 10.2148L9.99459 12.5714L8.01172 11.1669L9.69513 10.2148Z" fill="#EB8F35" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M7.899 7.78394L7.71289 11.0115L8.27123 9.36396L7.899 7.78394Z" fill="#EA8E3A" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M11.4187 10.2554L9.69531 10.2148L9.99474 12.5714L11.4187 10.2554Z" fill="#D87C30" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M13.1747 13.5977L9.99414 12.5714L11.4181 10.2554L13.9998 10.4444L13.1747 13.5977Z" fill="#EB8F35" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M8.12467 4.81982L9.73488 6.19051L10.9659 7.70977L7.89844 7.82457L8.12467 4.81982Z" fill="#E8821E" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M9.99459 12.5712L8.01172 11.1667L8.15811 12.3822V13.0641L9.50884 12.8008L9.99459 12.5712Z" fill="#DFCEC3" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M4.00586 12.5712L5.95544 11.1667L5.80241 12.3822V13.0641L4.45832 12.8008L4.00586 12.5712Z" fill="#DFCEC3" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M8.72404 8.46582L8.31152 9.34362L9.7687 8.95871L8.72404 8.46582Z" fill="#393939" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M13.2153 0.910156L8.125 4.81969L8.98337 2.69274L13.2153 0.910156Z" fill="#E88F35" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M13.2148 0.910156L13.8869 2.99659L13.5143 5.27884L13.7737 5.42742L13.4011 5.77177L13.7006 6.0351L13.288 6.41324L13.5475 6.64279L12.9487 7.39905L10.1407 6.52804C8.77003 5.4139 8.09134 4.84673 8.11794 4.81969C8.14458 4.7927 9.84794 3.48952 13.2148 0.910156Z" fill="#8E5A30" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M1.05131 7.36535L0 10.4444L2.62162 10.2891H4.30508V8.95889L4.23188 6.23096L3.85926 6.52806L1.05131 7.36535Z" fill="#F89D35" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M3.03516 7.70947L6.10259 7.78372L5.76992 9.37726L4.30605 8.99913L3.03516 7.70947Z" fill="#D87C30" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M3.03516 7.74316L4.30605 8.95854V10.174L3.03516 7.74316Z" fill="#EA8D3A" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M4.30566 8.99902L5.80279 9.37711L6.28854 11.0112L5.95582 11.2003L4.30566 10.2144V8.99902Z" fill="#F89D35" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M4.30533 10.2148L4.00586 12.5714L5.95548 11.2007L4.30533 10.2148Z" fill="#EB8F35" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M6.10078 7.78394L6.2869 11.0115L5.72852 9.36396L6.10078 7.78394Z" fill="#EA8E3A" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M2.58203 10.2554L4.30543 10.2148L4.006 12.5714L2.58203 10.2554Z" fill="#D87C30" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M0.825079 13.5977L4.00565 12.5714L2.58172 10.2554L0 10.4444L0.825079 13.5977Z" fill="#EB8F35" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M5.87636 4.81982L4.26615 6.19051L3.03516 7.70977L6.10259 7.82457L5.87636 4.81982Z" fill="#E8821E" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M5.27611 8.46582L5.68862 9.34362L4.23145 8.95871L5.27611 8.46582Z" fill="#393939" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M0.785156 0.910156L5.87537 4.81969L5.01699 2.69274L0.785156 0.910156Z" fill="#E88F35" />
+        <path fillRule="evenodd" clipRule="evenodd" d="M0.785323 0.910156L0.113281 2.99659L0.485899 5.27884L0.226419 5.42742L0.599036 5.77177L0.299612 6.0351L0.71213 6.41324L0.452649 6.64279L1.0515 7.39905L3.85945 6.52804C5.23014 5.4139 5.90883 4.84673 5.88223 4.81969C5.85558 4.7927 4.15888 3.48952 0.785323 0.910156Z" fill="#8E5A30" />
+    </svg>
+));

756e8d38ba24462fca706e990f9e04c3b13ac1c6
Anthony Bobrov <anton.bobrov@hotmail.com>
2022-07-13 00:25:48 +0300

    BoxHeading: add nav prop

--- src/components/box/heading/index.tsx
+++ src/components/box/heading/index.tsx
@@ -1,16 +1,21 @@
-import React, { FC, PropsWithChildren } from 'react';
+import React, { FC, PropsWithChildren, ReactNode } from 'react';
 import styles from './styles.module.scss';
 
 export interface Props {
     heading: string;
+    nav?: ReactNode;
 }
 
 export const BoxHeading: FC<PropsWithChildren<Props>> = ({
     heading,
+    nav,
     children,
 }) => (
     <section>
-        <h2 className={styles.heading}>{heading}</h2>
+        <div className={styles.head}>
+            <h2 className={styles.heading}>{heading}</h2>
+            {nav && <div className={styles.nav}>{nav}</div>}
+        </div>
         {children}
     </section>
 );
